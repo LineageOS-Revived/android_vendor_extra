e5c9e3710bbdcdad60ed572adce1ca5a4cb757d5
Dominik Baronelli <dbaronelli@shiftphones.com>
2022-12-15 11:37:33 +0100

    Allow oem unlock (aka 'fastboot flashing unlock')
    Change-Id: Ic72bd1a4e98c372c380f17fbf00e2c315a60b331

--- product.mk
+++ product.mk
@@ -1,6 +1,7 @@
 # Signing
 ifneq (eng,$(TARGET_BUILD_VARIANT))
     PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/certs/releasekey
+    PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.oem_unlock_supported=1
 endif
 
 # Updater
