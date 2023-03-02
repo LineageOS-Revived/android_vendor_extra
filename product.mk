54830c4bd91b6b721df721befa73200143a9c16a
Ido Ben-Hur <idoybh2@gmail.com>
2023-03-02 21:57:53 +0200

    config: Conditionally enable dev keys signing
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- product.mk
+++ product.mk
@@ -1,7 +1,9 @@
 # Signing
 ifneq (eng,$(TARGET_BUILD_VARIANT))
+ifneq (,$(wildcard vendor/certs/releasekey.pk8))
     PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/certs/releasekey
     PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.oem_unlock_supported=1
+endif
 endif
 
 # Updater
