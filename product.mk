facb69c5f790e0ea8fe3d1e066a0f5ba4c0bc502
Ido Ben-Hur <idoybh2@gmail.com>
2024-03-13 13:35:09 +0200

    version: Sign every build variant but eng
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- product.mk
+++ product.mk
@@ -1,5 +1,7 @@
 # Signing
-PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/certs/releasekey
+ifneq (eng,$(TARGET_BUILD_VARIANT))
+    PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/certs/releasekey
+endif
 
 # Updater
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += lineage.updater.allow_downgrading=true
