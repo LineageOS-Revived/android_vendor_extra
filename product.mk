a438ffcffd0b878bfbbcb910d181db70c5ae5e88
SirRGB <sirrgb@proton.me>
2024-06-02 01:48:25 +0200

    notify user about signing state
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- product.mk
+++ product.mk
@@ -1,8 +1,11 @@
 # Signing
 ifneq (eng,$(TARGET_BUILD_VARIANT))
 ifneq (,$(wildcard vendor/certs/releasekey.pk8))
+    $(warning Using custom signing keys from vendor/certs/)
     PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/certs/releasekey
     PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.oem_unlock_supported=1
+else
+    $(warning Not using custom signing keys, play integrity will fail)
 endif
 endif
 
